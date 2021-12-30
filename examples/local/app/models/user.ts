73c6ed2c4b6fe723b2d67be248b780b1763bd2bd
Khoa Nguyen <hi@khoanguyen.me>
2021-12-30 11:51:28 +0700

    Reproduce UTF8 bug in verify data

--- examples/local/app/models/user.ts
+++ examples/local/app/models/user.ts
@@ -2,9 +2,10 @@ import { AuthorizationError } from "remix-auth";
 
 export interface User {
   email: string;
+  name: string;
 }
 
 export async function login(email: string, password: string): Promise<User> {
-  if (password === "abc123") return { email };
+  if (password === "abc123") return { email, name: "Nguyễn Văn A" };
   throw new AuthorizationError();
 }
